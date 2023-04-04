4e2ba68088b34217111bbfacc9c271ee76cb33ab
gaslam <44768183+gaslam@users.noreply.github.com>
2023-04-05 01:10:26 +0200

    Add animation changes and new command

--- Minigin/Command.cpp
+++ Minigin/Command.cpp
@@ -1,6 +1,7 @@
 #include "Command.h"
 #include "GameObject.h"
 #include <glm/fwd.hpp>
+#include "CharacterComponent.h"
 
 dae::Command::Command(GameObject* actor) :
 	m_Owner{actor}
@@ -9,7 +10,41 @@ dae::Command::Command(GameObject* actor) :
 
 void dae::MoveCommand::Execute(float deltaTime)
 {
+	GameObject* pOwner = GetCommandOwner();
+	if (!pOwner)
+	{
+		return;
+	}
 	Execute(m_Dir, deltaTime);
+
+	CharacterComponent* pCharacter = pOwner->GetComponent<CharacterComponent>();
+	if (!pCharacter)
+	{
+		return;
+	}
+
+	dae::CharacterComponent::CharacterState state{};
+	if (m_Dir.x > 0)
+	{
+		state = dae::CharacterComponent::moveRight;
+	}
+
+	if (m_Dir.x < 0)
+	{
+		state = dae::CharacterComponent::moveLeft;
+	}
+
+	if (m_Dir.y > 0)
+	{
+		state = dae::CharacterComponent::moveUp;
+	}
+
+	if (m_Dir.y < 0)
+	{
+		state = dae::CharacterComponent::moveDown;
+	}
+
+	pCharacter->SetState(state);
 }
 
 void dae::MoveCommand::Execute(glm::vec3& dir, float deltaTime)
@@ -32,4 +67,23 @@ void dae::MoveCommand::Execute(glm::vec3& dir, float deltaTime)
 	pos.z += (dir.z * speed) * deltaTime;
 
 	pTransform->SetLocalPosition(pos);
+}
+
+void dae::SetChacterToStateIdleCommand::Execute(float)
+{
+	GameObject* pOwner = GetCommandOwner();
+	if (!pOwner)
+	{
+		return;
+	}
+
+	CharacterComponent* pCharacter = pOwner->GetComponent<CharacterComponent>();
+	if (!pCharacter)
+	{
+		return;
+	}
+
+	auto characterState = CharacterComponent::CharacterState::idle;
+
+	pCharacter->SetState(characterState);
 }
